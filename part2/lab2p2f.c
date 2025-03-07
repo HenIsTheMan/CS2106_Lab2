2ad9018ebc10a7dac03457a4715c334e4917af03
HenIsTheMan <2169test2169@gmail.com>
2025-03-07 13:52:20 +0800

    lab2p2f.c (isolating a small part that somewhat works)

--- part2/lab2p2f.c
+++ part2/lab2p2f.c
@@ -18,15 +18,15 @@ int main() {
 
     int pipeFdArr[2]; //0 for reading end, 1 for writing end
 
-    if(pipe(pipeFdArr) < 0){
+    if(pipe(pipeFdArr) == -1){ //If pipe fails...
         perror("lab2p2f: ");
         return 1;
     }
     
     int forkResult = fork();
 
-    if(forkResult < 0){
-        printf("lab2p2f: Fork Failed!\n");
+    if(forkResult == -1){ //If fork fails...
+        perror("lab2p2f: ");
         return 1;
     }
 
@@ -34,22 +34,38 @@ int main() {
     if(forkResult > 0){ //Parent process (does talk as can wait, ./talk < results.out > results.out [(a < b) > c, > instead of >> so overwrite instead of append])
         wait(NULL); //WAIT, THEY DON'T LOVE U LIKE I LOVE U
 
-        int myFd = open("./results.out", O_RDWR); //O_RDWR flag for read and write
+        /*int myReadFd = open("./results.out", O_RDONLY);
+        int myWriteFd = open("./results.out", O_WRONLY);
 
-        dup2(myFd, STDIN_FILENO);
-        dup2(myFd, STDOUT_FILENO);
+        dup2(myReadFd, STDIN_FILENO);
+        dup2(myWriteFd, STDOUT_FILENO);
 
         execlp("./talk", "talk", NULL);
 
-        close(myFd);
+        close(myReadFd);
+        close(myWriteFd);*/
+
+        //        int savedStdoutFd = dup(STDOUT_FILENO);
+        //dup2(savedStdoutFd, STDOUT_FILENO); //Restore file descriptor of stdout
     } else{ //Child process (does slow as cannot wait, ./slow 5 > results.out)
         int myFd = open("./results.out", O_CREAT | O_WRONLY); //O_CREAT flag to create file if it does not alr exist
+
+        if(myFd == -1){ //If open fails...
+            perror("lab2p2f: ");
+            return 1;
+        }
 
         dup2(myFd, STDOUT_FILENO);
 
         execlp("./slow", "slow", "5", NULL);
 
+        //* If execlp fails... (only runs if so)
         close(myFd);
+
+        perror("lab2p2f: ");
+
+        return 1;
+        //*/
     }
 }
 
