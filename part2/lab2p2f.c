76c2dc65f5f899d74bd60f25a8a502f464af97fb
HenIsTheMan <2169test2169@gmail.com>
2025-03-07 13:53:10 +0800

    lab2p2f.c (still trying)

--- part2/lab2p2f.c
+++ part2/lab2p2f.c
@@ -36,38 +36,62 @@ int main() {
         return 1;
     }
 
+    int myFd = open("./results.out", O_CREAT | O_WRONLY); //O_CREAT flag to create file if it does not alr exist
+
+    if(myFd == -1){ //If open fails...
+        perror("\nOpen Failed!");
+
+        puts("");
+
+        return 1;
+    }
+
     ///"./" refers to the curr directory
     if(forkResult > 0){ //Parent process (does talk as can wait, ./talk < results.out > results.out [(a < b) > c, > instead of >> so overwrite instead of append])
-        wait(NULL); //WAIT, THEY DON'T LOVE U LIKE I LOVE U
+        int status;
 
-        int myReadFd = open("./results.out", O_RDONLY);
-        //int myWriteFd = open("./results.out", O_WRONLY);
+        if(wait(&status) == -1){ //WAIT, THEY DON'T LOVE U LIKE I LOVE U
+            close(myFd);
 
-        dup2(myReadFd, STDIN_FILENO);
-        //dup2(myWriteFd, STDOUT_FILENO);
-
-        execlp("./talk", "talk", NULL);
-
-        //        int savedStdoutFd = dup(STDOUT_FILENO);
-        //dup2(savedStdoutFd, STDOUT_FILENO); //Restore file descriptor of stdout
-    } else{ //Child process (does slow as cannot wait, ./slow 5 > results.out)
-        int myFd = open("./results.out", O_CREAT | O_WRONLY); //O_CREAT flag to create file if it does not alr exist
-
-        if(myFd == -1){ //If open fails...
-            perror("\nOpen Failed!");
+            perror("\nWait Failed!");
 
             puts("");
 
             return 1;
         }
 
+        close(myFd); //Must be after wait as child uses myFd, must be before status check as still need to close myFd if child's execlp failed
+
+        if(status == 1){ //If child's execlp failed... (printing alr done by child)
+            return 1;
+        }
+
+        myFd = open("./results.out", O_RDWR); //??
+
+        dup2(myFd, STDIN_FILENO);
+        dup2(myFd, STDOUT_FILENO);
+
+        execlp("./talk", "talk", NULL);
+
+        //??
+
+
+
+
+        //int myReadFd = open("./results.out", O_RDONLY);
+        //int myWriteFd = open("./results.out", O_WRONLY);
+
+        //dup2(myReadFd, STDIN_FILENO);
+        //dup2(myWriteFd, STDOUT_FILENO);
+
+        //        int savedStdoutFd = dup(STDOUT_FILENO);
+        //dup2(savedStdoutFd, STDOUT_FILENO); //Restore file descriptor of stdout
+    } else{ //Child process (does slow as cannot wait, ./slow 5 > results.out)
         dup2(myFd, STDOUT_FILENO);
 
         execlp("./slow", "slow", "5", NULL);
 
         //* If execlp fails... (only runs if so)
-        close(myFd);
-
         perror("\nExeclp Failed!");
 
         puts("");
